a9f96cca789adad375da3824d6b72f33664975c1
Leereal <leereal08@ymail.com>
2022-06-30 04:39:55 +0200

    Removed Payment Method on Banking details

--- resources/views/backend/user/banking_detail/create.blade.php
+++ resources/views/backend/user/banking_detail/create.blade.php
@@ -25,31 +25,35 @@
 								<label class="control-label">{{ _lang('Branch') }}</label>
 								<input type="text" class="form-control" name="branch" value="{{ old('branch') }}">
 							</div>
-						</div>                     
+						</div>
 
 						<div class="col-md-12">
 							<div class="form-group">
 								<label class="control-label">{{ _lang('Account Type') }}</label>
-								<input type="text" class="form-control" name="account_type" value="{{ old('account_type') }}">
-							</div>
-						</div>  
-                        
-                        <div class="col-md-12">
-							<div class="form-group">
-								<label class="control-label">{{ _lang('Account Number') }}</label>
-								<input type="text" class="form-control" name="account_number" value="{{ old('account_number') }}">
-							</div>
-						</div> 
-
-                        <div class="col-md-12">
-							<div class="form-group">
-								<label class="control-label">{{ _lang('Payment Method') }}</label>
-								<select class="form-control auto-select select2" data-selected="{{ old('withdraw_method_id') }}" name="withdraw_method_id" required>
-									<option value="">{{ _lang('Select One') }}</option>
-									{{ create_option('withdraw_methods','id','name',old('withdraw_method_id'), array('status=' => 1)) }}
-								</select>
+								<input type="text" class="form-control" name="account_type"
+									value="{{ old('account_type') }}">
 							</div>
 						</div>
+
+						<div class="col-md-12">
+							<div class="form-group">
+								<label class="control-label">{{ _lang('Account Number') }}</label>
+								<input type="text" class="form-control" name="account_number"
+									value="{{ old('account_number') }}">
+							</div>
+						</div>
+
+						{{-- <div class="col-md-12">
+							<div class="form-group">
+								<label class="control-label">{{ _lang('Payment Method') }}</label>
+								<select class="form-control auto-select select2"
+									data-selected="{{ old('withdraw_method_id') }}" name="withdraw_method_id" required>
+									<option value="">{{ _lang('Select One') }}</option>
+									{{ create_option('withdraw_methods','id','name',old('withdraw_method_id'),
+									array('status=' => 1)) }}
+								</select>
+							</div>
+						</div> --}}
 
 
 						<div class="col-md-12">
@@ -64,5 +68,3 @@
 	</div>
 </div>
 @endsection
-
-
